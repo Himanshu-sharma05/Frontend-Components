20fb7a9323e79d2493f781a7a5b325fbe67a99bd
Himanshu <himanshusharma052008@gmail.com>
2025-03-20 00:13:21 +0530

    completed the animated menu icon

--- FrontendComponents/src/components/HoverMenu.jsx
+++ FrontendComponents/src/components/HoverMenu.jsx
@@ -14,23 +14,24 @@ const AnimatedButton = ()=>{
     const [active, setactive] = useState(false);
     return(<motion.button
         onClick={()=> setactive((prev)=> !prev)}
-    className="relative h-20 w-20 bg-black ml-[40%] mt-50 cursor-pointer hover:bg-amber-200 rounded-full transition-colors"
+    className="relative h-20 w-20 bg-black ml-[40%] mt-50 cursor-pointer hover:bg-blue-700 rounded-full transition-colors"
      animate = {active ? "open": "closed"}
     >
         <motion.span
+        initial={false}
         variants={{
             open:{
-                rotate:"45deg",
+                rotate:["0deg","0deg","45deg"],
                 top:["35%","50%","50%"]
             },
             closed:{
-                rotate:"0deg",
+                rotate:["45deg","0deg","0deg"],
                 top:["50%","50%","35%"]
             }
         }}
 
         transition={{
-            duration:1,
+            duration:0.5,
             ease:"easeInOut"
         }}
         style={{
@@ -50,7 +51,7 @@ const AnimatedButton = ()=>{
             }
         }}
         transition={{
-            duration:1,
+            duration:0.5,
             ease:"easeInOut"
         }}
         style={{
@@ -59,8 +60,20 @@ const AnimatedButton = ()=>{
         className='h-1 w-15 bg-white absolute '
         />
         <motion.span
+        variants={{
+            open:{
+                rotate:["0deg","0deg","45deg"],
+                left:"50%",
+                bottom:["30%","45%","45%"]
+            },
+            closed:{
+                rotate:["45deg","0deg","0deg"],
+                bottom:["45%","45%","30%"]
+                
+            }
+        }}
         transition={{
-            duration:1,
+            duration:0.5,
             ease:"easeInOut"
         }}
         style={{
